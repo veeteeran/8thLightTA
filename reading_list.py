86548c8dd7125fd9f9870d44608f0a19eff63497
Viet <vietnamtran@gmail.com>
2021-06-23 15:36:16 -0500

    Clean up reading_list file

--- reading_list.py
+++ reading_list.py
@@ -1,34 +1,13 @@
 #!/usr/bin/env python3
-""" This app searches for books and constructs a reading list"""
-from utils import get_books, greet
+""" This class constructs a reading list using the Books class"""
+
+from models.books import Books 
 import json
 import os
-from requests import get
-
-'''
-  ReadList responsibilities:
-  - gets user input
-    - prompts user
-    - returns user input
-  - print each book in the reponse object
-    - prints (book) volume_info
-  - http request
-    - ping API
-    - Validate response
-    - returns a list of dictionaries
-  - prompts user
-    - save_books_prompt
-  - saves_to_list
-    - prompts user to save
-    - validate input
-    - write to file
-  - shows_list
-    - print reading list
-'''
   
 class ReadingList:
   """
-    Contains attributes and methods to run reading list application
+    Contains attributes and methods for ReadList class
     
     ...
 
@@ -41,21 +20,12 @@ class ReadingList:
 
     Methods
     -------
-    *** get_input and get_books, print_book_info and save_books_prompt class ***
     get_input():
       Prompts user and returns user input
-
-    print_book_info(book, count = 1):
-      Prints user search to screen
-
-    get_books(search_terms):
-      Returns a list of five dictionary items and prints to screen
-      author, title, and publisher
 
     save_books_prompt():
       Prompts user to save book or exit from book search
 
-    *** save_to_list and show_list could be class List ***
     save_to_list(books):
       Saves books to reading_list
 
@@ -82,10 +52,9 @@ class ReadingList:
     user_input : str - search string entered from command line
     """
 
-    # print("\nHello! Enter a book or author to start")
-    # print("Type the word 'list' to see your reading list")
-    # print("Or type the word 'exit' to leave\n")
-    greet()
+    print("\nHello! Enter a book or author to start")
+    print("Type the word 'list' to see your reading list")
+    print("Or type the word 'exit' to leave\n")
 
     user_input = input()
     if user_input == 'exit':
@@ -97,83 +66,6 @@ class ReadingList:
 
     print(f'\nSearching for {user_input}...\n')
     return user_input
-
-  def print_book_info(self, book, count=1):
-    """
-    Prints user search to screen
-    
-    Parameters
-    ----------
-    book : dictionary
-        volumeInfo passed in from the response object
-    
-    count : int
-        number to print in front of book
-
-    Returns
-    -------
-    None
-    """
-
-    print(f"{count}. ", end='')
-
-    if not book.get('authors'):
-      print('None', end = ', ')
-    elif len(book.get('authors')) > 1:
-      separator = ', '
-      print(f"{separator.join(book.get('authors'))}", end=', ')
-    else:
-      print(f"{book.get('authors')[0]}", end=', ')
-
-    print(book.get('title'), end=', ')
-    print(book.get('publisher'))
-
-  def get_books(self, search_terms):
-    """
-    Gets a list of five books based on user search
-
-    Parameters
-    ----------
-    search_terms : str
-        search string from user
-
-    Returns
-    -------
-      List of five dictionary items and prints to screen
-      author, title, and publisher
-    """
-
-    books = []
-
-    url = 'https://www.googleapis.com/books/v1/volumes?q='
-
-    max_results = '&maxResults=5'
-
-    response = get(url + search_terms + max_results)
-    # print(response.text)
-    if not response:
-      print('\nAn error has occurred.\n')
-
-    items = response.json().get('items')
-    # print(f"ITEMS: {items}")
-    if not items:
-      print(f'\nSorry, nothing found for {search_terms}\n')
-      return
-    
-    for count, item in enumerate(items, 1):
-      my_dict = {}
-      volume_info = item.get('volumeInfo')
-      self.print_book_info(volume_info, count)
-
-      my_dict = {
-        'authors': volume_info.get('authors'),
-        'title': volume_info.get('title'),
-        'publisher': volume_info.get('publisher')
-      }
-
-      books.append(my_dict)
-    # print(books)
-    return books
 
   def save_books_prompt(self):
     """
@@ -214,19 +106,20 @@ class ReadingList:
       return False
 
     valid_input = [count for count, book in enumerate(books, 1)]
+    b = Books()
     try:
       if int(user_input) not in valid_input:
         print(f'\n{user_input} is not in the list.')
         print('Please make another selection.\n')
         for count, book in enumerate(books, 1):
-          self.print_book_info(book, count)
+          b.print_book_info(book, count)
       else:
         print('\n*********************************')
         print(f'* Item {user_input} saved to reading list! *')
         print('*********************************\n')
         to_add = books.pop(int(user_input) - 1)
         for count, book in enumerate(books, 1):
-          self.print_book_info(book, count)
+          b.print_book_info(book, count)
 
         if os.stat(self.__file_path).st_size != 0:
           with open(self.__file_path, 'r') as read_file:
@@ -238,7 +131,7 @@ class ReadingList:
     except ValueError:
       print(f'\n{user_input} is not a number\n')
       for count, book in enumerate(books, 1):
-        self.print_book_info(book, count)
+        b.print_book_info(book, count)
 
     return True
 
@@ -256,11 +149,12 @@ class ReadingList:
     """
 
     with open(self.__file_path, 'r') as read_file:
+      b = Books()
       try:
         books = json.load(read_file)
         print("\nHere's your list!\n")
         for count, book in enumerate(books, 1):
-          self.print_book_info(book, count)
+          b.print_book_info(book, count)
       except json.decoder.JSONDecodeError as e:
         print('\nYour list is empty\n')
 
@@ -280,8 +174,8 @@ class ReadingList:
     while True:
       user_input = self.get_input()
       if user_input != 'list':
-        books = self.get_books(user_input)
-        # books = get_books(user_input)
+        b = Books()
+        books = b.get_books(user_input)
         if books != []:
           is_selecting = self.save_to_list(books)
           while is_selecting:
